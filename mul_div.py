021af148a8a90bcdc57e0e43400c3cf92c293aca
menastudy <menakadinesh13@gmail.com>
2024-11-24 22:33:32 -0800

    Create mul_div.py
    This is a python program that get 2 numbers from users and  multiply and divide the numbers

---
+++ mul_div.py
@@ -0,0 +1,12 @@
+#Get input from users#
+print("Enter numbers to multiply")
+num1=input("Enter number 1")
+num2=input("Enter number 2")
+
+#Code to multiply 2 numbers###
+mul_result= int(num1) * int(num2)
+print("Multiplication of 2 numbers:",mul_result)
+
+#Code to divide 2 number####
+div_result=float(num1)/float(num2)
+print("Divison of 2 numbers:",div_result)
