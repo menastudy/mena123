f64c8f33ddcb9207fb1f74b8be50de46a746973f
menastudy <menakadinesh13@gmail.com>
2024-11-24 22:17:33 -0800

    Create add_sub.py

---
+++ add_sub.py
@@ -0,0 +1,11 @@
+#Get input from users#
+print("Enter numbers to add")
+num1=input("Enter number 1")
+num2=input("Enter number 2")
+#Code to add two numbers###
+sum = int(num1) + int(num2)
+print("Sum of 2 numbers:",sum)
+#Code to substract 2 number#
+sub=int(num1)-int(num2)
+print("Substraction of 2 numbers:",sub)
+
